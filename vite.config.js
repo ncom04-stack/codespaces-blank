232fde9188afad45f767a9a59146312c9b8afe13
ncom04-stack <nexicom04@gmail.com>
2026-01-28 16:14:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // This is the fix for the "First Screen Only" issue
+  plugins: [
+    react(),
+    tailwindcss(), // Add this to the plugins array
+  ],
+  base: '/',
 })
