065c0a60fa22e5fe60a392ab538431a9a05413de
ncom04-stack <nexicom04@gmail.com>
2026-01-28 16:02:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'X_charge', // Replace with your actual GitHub repository name
+  // Ensure this is exactly '/' for Vercel
+  base: '/', 
 })
